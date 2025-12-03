805d4d441160ae8c4e769b4cd845461f3152a018
Danilo <danilo.merlo@sou.inteli.edu.br>
2025-12-03 19:37:33 -0300

    feat: functions

--- src/front.py
+++ src/front.py
@@ -6,6 +6,32 @@ def stream_text(text):
         yield char
         time.sleep(0.02)
 
+def user_text(input_text):
+    files_info = []
+    
+    if input_text.files:
+        for f in input_text.files:
+            files_info.append({"name": f.name, "type": f.type, "data": f.read()})
+
+    st.session_state["messages"].append({"role": "user", "content": input_text.text, "avatar": "assets/thunderatz.png", "files": files_info})
+
+    user = st.chat_message("user", avatar="assets/thunderatz.png")
+    
+    for f in files_info:
+        if f["type"].startswith("image/"):
+            user.image(f["data"])
+        elif f["type"] == "application/pdf":
+            user.download_button(f["name"], f["data"], f["name"])
+        else:
+            user.write(f"Arquivo enviado: {f['name']}")
+
+    user.write(input_text.text)
+
+def ia_response(response):
+    st.session_state["messages"].append({"role": "assistant", "content": response, "avatar": "ai"})
+    ai = st.chat_message("assistant", avatar="ai")
+    ai.write_stream(stream_text(response))
+
 if "messages" not in st.session_state:
     st.session_state["messages"] = []
 
@@ -23,37 +49,13 @@ for msg in st.session_state["messages"]:
                 chat.write(f"Arquivo enviado: {f['name']}")
 
 
-text_input = st.chat_input(placeholder="Your message", key=None, max_chars=None, accept_file="multiple", file_type=None, disabled=False, on_submit=None, args=None, kwargs=None, width="stretch")
+input_box = st.chat_input(placeholder="Your message", key=None, max_chars=None, accept_file="multiple", file_type=None, disabled=False, on_submit=None, args=None, kwargs=None, width="stretch")
 
-if text_input:
-    files_info = []
+if input_box:
 
-    if text_input.files:
-        for f in text_input.files:
-            files_info.append({"name": f.name, "type": f.type, "data": f.read()})
-
-    st.session_state["messages"].append({"role": "user", "content": text_input.text, "avatar": "assets/thunderatz.png", "files": files_info})
-
-    user = st.chat_message("user", avatar="assets/thunderatz.png")
-    
-    for f in files_info:
-        if f["type"].startswith("image/"):
-            user.image(f["data"])
-        elif f["type"] == "application/pdf":
-            user.download_button(f["name"], f["data"], f["name"])
-        else:
-            user.write(f"Arquivo enviado: {f['name']}")
-    user.write(text_input.text)
+    user_text(input_box)
 
     time.sleep(1)
-
-    ai_response = "A THUNDERATZ <3<3<3"
-
-    st.session_state["messages"].append({
-        "role": "assistant",
-        "content": ai_response,
-        "avatar": "ai"
-    })
-
-    ai = st.chat_message("assistant", avatar="ai")
-    ai.write_stream(stream_text(ai_response))
+    
+    response = "Thunderatz <3<3<3"
+    ia_response(response)    
